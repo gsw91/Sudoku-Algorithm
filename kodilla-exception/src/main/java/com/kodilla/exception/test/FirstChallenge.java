43a4a82f6eeffec7562682dfdf7fb4abbdc6d2d7
gsw91 <g.wojcik@vp.pl>
2017-12-30 19:19:21 +0100

    Zadanie 8.2

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,36 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+
+        double c;
+            try {
+                if (b==0) {
+                    throw new ArithmeticException();
+                } else {
+                    c = a/b;
+                }
+                return c;
+            } catch (ArithmeticException e) {
+                System.out.println("Error: " + e);
+                throw new ArithmeticException();
+            } finally {
+                System.out.println("Keep going! Never give up!");
+            }
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        double result = firstChallenge.divide(3, 0);
+
+        System.out.println(result);
+    }
+}
