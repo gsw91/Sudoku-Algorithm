f07263bf8f90764fa4df086ea175fba6e567cf75
gsw91 <g.wojcik@vp.pl>
2018-06-19 15:44:19 +0200

    Sudoku tests update

---
+++ sudoku-app/src/test/java/com/sudoku/SetupGame/GameConfigurationTest.java
@@ -0,0 +1,34 @@
+package com.kodilla.sudoku.SetupGameTests;
+
+import com.sudoku.Board.*;
+import com.sudoku.ExamplesBoards.*;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class GameConfigurationTest {
+
+    @Ignore
+    @Test
+    public void makeDeepCopyOfBoard() {
+        //given
+        //GameConfiguration gameConfiguration = GameConfiguration.getInstance();
+        ExamplesBoards examplesBoards = new ExamplesBoards();
+        SudokuBoard sudokuBoard = examplesBoards.getBoardLevelhardOne();
+        //when
+        //gameConfiguration.deepCopyInGame(sudokuBoard, 1, 2, 2);
+        sudokuBoard.getSudokuRow(1).get(2).setValue(2);
+        sudokuBoard.getSudokuRow(2).get(1).setValue(3);
+        //SudokuBoard clonedBoard = gameConfiguration.loadLastSavedCopy();
+        //then
+        //Assert.assertEquals(-1, clonedBoard.getSudokuRow(1).get(2).getValue());
+        //Assert.assertEquals(-1, clonedBoard.getSudokuRow(2).get(1).getValue());
+        Assert.assertEquals(2, sudokuBoard.getSudokuRow(1).get(2).getValue());
+        Assert.assertEquals(3, sudokuBoard.getSudokuRow(2).get(1).getValue());
+        //else
+        //sudokuBoard = gameConfiguration.loadLastSavedCopy();
+        Assert.assertEquals(-1, sudokuBoard.getSudokuRow(1).get(2).getValue());
+        Assert.assertEquals(-1, sudokuBoard.getSudokuRow(2).get(1).getValue());
+
+    }
+}
